bd64915ba87ce0f1a3dd9b96bcc28276fe6b58b2
Punnawich Mai <punnawich_dachin@gmail.com>
2024-10-11 17:15:57 +0700

    Handle Error

--- src/src/Main.java
+++ src/src/Main.java
@@ -3,11 +3,11 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Assembler comb = new Assembler("src/program/Combination/Combination.txt","src/program/Combination/MachineCode.txt","src/program/Combination/result.txt");
-        comb.ConvertAndSim();
+//        Assembler comb = new Assembler("src/program/Combination/Combination.txt","src/program/Combination/MachineCode.txt","src/program/Combination/result.txt");
+//        comb.ConvertAndSim();
 
-//        Assembler div = new Assembler("src/program/Division/Division.txt","src/program/Division/MachineCode.txt","src/program/Division/result.txt");
-//        div.ConvertAndSim();
+        Assembler div = new Assembler("src/program/Division/Division.txt","src/program/Division/MachineCode.txt","src/program/Division/result.txt");
+        div.ConvertAndSim();
 
 //        Assembler mul = new Assembler("src/program/multiplication/multiplication.txt","src/program/multiplication/MachineCode.txt","src/program/multiplication/result.txt");
 //        mul.ConvertAndSim();
